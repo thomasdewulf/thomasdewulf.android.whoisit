8bad4f08e6b5389e19905c33f467bdfec2531d43
Thomas de Wulf <thomas.de.wulf@telenet.be>
2017-11-13 14:03:13 +0100

    Initial commit

---
+++ app/src/main/java/be/thomasdewulf/whoisit/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package be.thomasdewulf.whoisit.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import be.thomasdewulf.whoisit.R;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
